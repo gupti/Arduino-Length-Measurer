f99d877049e1f2777c813a502a44663bb35018da
gupti <gupti@airmail.cc>
2016-03-08 19:19:05 -0500

    Fixes and additions

--- _201_Project.ino
+++ _201_Project.ino
@@ -1,29 +1,88 @@
+// MTE 201 Project
+// Program for a distance measurement device using a potentiomenter (linear)
+
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
+typedef unsigned char byte;
 
 // Defines for EEPROM locations
-#define slope 0
-#define offset 4
+#define SLOPE 0
+#define OFFSET 4
+#define ERR 8
+#define BTNTIME 1500
+const uint8_t BUTTONLIST[] = {6, 7, 8};
 
-LiquidCrystal display(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
 {
+  float f = 0.00f;
   lcd.begin(16, 2);
+
+  // Setup button pins and measurement pins
+  // Pin A0 is for the measurement probe
+  for (int i = 0; i < sizeof(BUTTONLIST)/sizeof(uint8_t); i++)
+  {
+    pinMode(i, INPUT);
+  }
+  
+  // If the slope is zero, the device is not calibrated [properly]
+  if (abs(EEPROM.get(SLOPE, f)) < 0.00001)
+  {
+    // Clear EEPROM
+    for (int i = 0; i < sizeof(float)*3; i += sizeof(float))
+    {
+      EEPROM.put(i, 0.00f);
+    }
+    do
+    {
+      lcd.write("Calibration req.");
+      lcd.setCursor(0, 1);
+      lcd.write("Push btn to cont");
+      handleButtons();
+    } while(!calibrate());
+  }
+}
+// Handles button input
+// Returns negative if button is hed for certain amount of time and onlift is true
+int handleButtons()
+{
+  bool x = true;
+  uint8_t i = 0;
+  unsigned long buttonHeld = 0;
+  while(x)
+  {
+    for (i = 0; i < sizeof(BUTTONLIST)/sizeof(uint8_t); i++)
+    {
+      if (digitalRead(BUTTONLIST[i]))
+      {
+        x = false;
+        break;
+      }
+    }
+  }
+  
+  buttonHeld = millis();
+  while(digitalRead(BUTTONLIST[i]));
+  if (millis() - buttonHeld >= BTNTIME)
+  {
+    return -i;
+  }
+  return i;
+}
+
+bool calibrate()
+{
+  return 0;
 }
 
 // Datapoints, stepSize, y is the point where the step starts
-double findSlope(uint16_t *datapoints, uint8_t stepSize, uint8_t stepStart)
+float findSlope(uint8_t numberOfPoints, uint16_t *datapoints, uint8_t stepSize, uint8_t stepStart)
 {
-  // if smaller than 2 exit
-  if (!(steps & 0xfe))
-  {
-    return 0;
-  }
   unsigned long x = 0, y = 0, xy = 0, xx = 0;
   int i;
 
-  for (i = 0; i < datapoints.length(); i++)
+  for (i = 0; i < numberOfPoints; i++)
   {
     x += datapoints[i];
     y += i * stepSize + stepStart;
@@ -31,7 +90,7 @@ double findSlope(uint16_t *datapoints, uint8_t stepSize, uint8_t stepStart)
     xx += datapoints[i]*datapoints[i];
   }
 
-  return ((float)datapoints.length()*xy - x*y)/(datapoints.length()*xx - x*x);
+  return ((float)numberOfPoints*xy - x*y)/(numberOfPoints*xx - x*x);
 }
 
 void loop()
